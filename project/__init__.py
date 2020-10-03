25cee0968d4fbe2e8d0725776e049d1c513fed0f
safikh <safiuddin.khaja@gmail.com>
2020-10-03 18:08:42 +0530

    Environment variables set

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from project.core.views import core
 from project.error_pages.handlers import error_pages
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
+app.config['SECRET_KEY'] = os.environ.get('FORM_SECRET_KEY', None)
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
